661df62e221e7f03b695c2c73541227d98634f25
Rodion Kochetkov <www.radik@yandex.by>
2019-03-24 23:00:46 +0300

    solution: all tests is not completed, some problem with |

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let l = str.length,
+    char,
+    last,
+    stack = [];
+
+  for (let i = 0; i < l; i++) {
+    char = str[i];
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (char == bracketsConfig[j][0]) {
+        stack.push(char);
+      } else if (char == bracketsConfig[j][1]) {
+        if (stack.length > 0) {
+          last = stack[stack.length - 1];
+          console.log("last is", last);
+          console.log("char is", char);
+          if (char == bracketsConfig[j][1] && last == bracketsConfig[j][0]) {
+            stack.pop();
+          }
+        }
+      }
+    }
+  }
+
+  return stack.length == 0;
 }
+
+/* function isBalanced(str) {
+  let checkString = str;
+  let stack = [];
+
+  // Если входная строка пуста, то технически всё сбалансировано
+  if (checkString.length <= 0) return true;
+
+  for (let i = 0; i < checkString.length; i++) {
+    if (checkString[i] === '{') {
+      stack.push(checkString[i]);
+    } else if (checkString[i] === '}') {
+      if (stack.length > 0) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  if (stack.pop()) return false;
+  return console.log(true);
+};
+ */
