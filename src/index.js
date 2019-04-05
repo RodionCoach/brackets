8e3a1ae6cf702cba32530ded4f0fde82a1bcd4ec
Rodion Kochetkov <www.radik@yandex.by>
2019-04-05 19:29:08 +0300

    fix: 2 test are not completed

--- src/index.js
+++ src/index.js
@@ -2,49 +2,55 @@ module.exports = function check(str, bracketsConfig) {
   let l = str.length,
     char,
     last,
+    /* flag = 0, */
     stack = [];
 
   for (let i = 0; i < l; i++) {
     char = str[i];
     for (let j = 0; j < bracketsConfig.length; j++) {
-      if (char == bracketsConfig[j][0]) {
+      /*       if(char == '|' || char == '7' || char == '8'){
+              flag = 1;
+              stack.push(char);
+            } */
+      if (char == bracketsConfig[j][0] /* && flag == 0 */ ) {
         stack.push(char);
       } else if (char == bracketsConfig[j][1]) {
         if (stack.length > 0) {
-          last = stack[stack.length - 1];
-          console.log("last is", last);
-          console.log("char is", char);
-          if (char == bracketsConfig[j][1] && last == bracketsConfig[j][0]) {
+          if (stack[stack.length - 1] == bracketsConfig[j][0]) {
             stack.pop();
+            /* flag = 0; */
           }
+        } else {
+          console.log('Char',char);
+          stack.push(char);
         }
       }
     }
-  }
-
-  return stack.length == 0;
-}
-
-/* function isBalanced(str) {
-  let checkString = str;
-  let stack = [];
-
-  // Если входная строка пуста, то технически всё сбалансировано
-  if (checkString.length <= 0) return true;
-
-  for (let i = 0; i < checkString.length; i++) {
-    if (checkString[i] === '{') {
-      stack.push(checkString[i]);
-    } else if (checkString[i] === '}') {
-      if (stack.length > 0) {
-        stack.pop();
-      } else {
-        return false;
+    if (stack.reduce((a, b) => (a[b] = a[b] + 1 || 1) && a, {})['|'] % 2 == 0) {
+      let index = 0;
+      while (index !== -1) {
+        index = stack.indexOf('|');
+        if(index !== -1) stack.splice(index, 1);
+      }
+    }
+    if (stack.reduce((a, b) => (a[b] = a[b] + 1 || 1) && a, {})['7'] % 2 == 0) {
+      let index = 0;
+      while (index !== -1) {
+        index = stack.indexOf('7');
+        if(index !== -1) stack.splice(index, 1);
+      }
+    }
+    if (stack.reduce((a, b) => (a[b] = a[b] + 1 || 1) && a, {})['8'] % 2 == 0) {
+      let index = 0;
+      while (index !== -1) {
+        index = stack.indexOf('8');
+        if(index !== -1) stack.splice(index, 1);
       }
     }
   }
 
-  if (stack.pop()) return false;
-  return console.log(true);
-};
- */
+  if (0) {
+
+  }
+  return stack.length == 0;
+}
